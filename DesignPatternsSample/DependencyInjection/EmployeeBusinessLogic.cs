2337dec02e93e30de0dfb934368bfb78ca896605
Kyaw Myo Aung <info.kyawmyo@gmail.com>
2020-02-06 14:13:27 +0630

    DI with constructor, properties, method

--- DesignPatternsSample/DependencyInjection/EmployeeBusinessLogic.cs
+++ DesignPatternsSample/DependencyInjection/EmployeeBusinessLogic.cs
@@ -9,13 +9,29 @@ namespace DesignPatternsSample.DependencyInjection
     public class EmployeeBusinessLogic
     {
         private IEmployeeDataAccess _dataAccess;
+
+        public EmployeeBusinessLogic()
+        {
+        }
+
         public EmployeeBusinessLogic(IEmployeeDataAccess dataAccess)
         {
             _dataAccess = dataAccess;
         }
 
+        public IEmployeeDataAccess EmployeeDataAccess { get;  set; }
+
         public List<Employee> GetEmployees()
         {
+            return _dataAccess.GetEmployees();
+        }
+
+
+        //method injeciton
+        public List<Employee> GetEmployees(IEmployeeDataAccess employeeDataAccess)
+        {
+            _dataAccess = employeeDataAccess;
+
             return _dataAccess.GetEmployees();
         }
     }
--- DesignPatternsSample/Program.cs
+++ DesignPatternsSample/Program.cs
@@ -13,13 +13,25 @@ namespace DesignPatternsSample
     {
         static void Main(string[] args)
         {
-            //constructor  dependency injeciton
+            //constructor  dependency injection
             EmployeeBusinessLogic employeeBusinessLogic = new EmployeeBusinessLogic(new EmployeeDataAccess());
 
             var employees = employeeBusinessLogic.GetEmployees();
-
             employees.ForEach(x => Console.WriteLine(x.FullName));
+            Console.WriteLine("---------");
 
+            //properties dependency injection
+            employeeBusinessLogic.EmployeeDataAccess =new EmployeeDataAccess();
+            var employeeList = employeeBusinessLogic.EmployeeDataAccess.GetEmployees();
+            employeeList.ForEach(x => Console.WriteLine(x.FullName));
+            Console.WriteLine("---------");
+
+            //method injection
+            EmployeeBusinessLogic businessLogic=new EmployeeBusinessLogic();
+            var _employeeList = businessLogic.GetEmployees(new EmployeeDataAccess());
+            _employeeList.ForEach(x => Console.WriteLine(x.FullName));
+           
+           
             Console.ReadLine();
         }
     }
